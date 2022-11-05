5e44c18984665cf468ccbfbfde88523462024125
mardochee01 <mardoche.gneran@gmail.com>
2022-11-05 12:24:36 +0000

    task update

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
-
-"""API REDIT
+"""
+0-subs function that queries the Reddit API
+and returns the number of subscribers
 """
 import requests
+
 
 def number_of_subscribers(subreddit):
     """If not a valid subreddit, return 0."""
